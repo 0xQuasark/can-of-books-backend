e69416e86eefe97331ce81d3d8d9fc5f824c364a
Quasark <q@quasark.xyz>
2023-08-28 13:00:58 -0700

    deploy test

--- server.js
+++ server.js
@@ -3,14 +3,23 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
+
+const PORT = process.env.PORT || 3001;
+const MONGODB_URL = process.env.MONGODB_URL;
 
 const app = express();
 app.use(cors());
 
-const PORT = process.env.PORT || 3001;
+mongoose.connect(MONGODB_URL);
+
 
 app.get('/test', (request, response) => {
   response.send('test request received')
+})
+
+app.get('/books', (request, response) => {
+  response.send('Books request received')
 })
 
 
